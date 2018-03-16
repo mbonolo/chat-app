117fde553a3e4d64977f6172a06686233b73b9ce
Marco Bonolo <bonolomarco@gmail.com>
2018-03-16 23:26:36 +0100

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e) {
@@ -32,12 +37,14 @@ jQuery('#message-form').on('submit', function(e) {
 
   var messageTextBox = jQuery('[name=message]');
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: messageTextBox.val()
-  }, function() {
-    messageTextBox.val('');
-  });
+  if(messageTextBox.val() !== '') {
+    socket.emit('createMessage', {
+      from: 'User',
+      text: messageTextBox.val()
+    }, function() {
+      messageTextBox.val('');
+    });
+  }
 });
 
 var locationButton = jQuery('#send-location');
