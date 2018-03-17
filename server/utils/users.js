9f32b2262c7cd2722a69cec76618592f61ca3f18
Marco Bonolo <bonolomarco@gmail.com>
2018-03-17 19:19:45 +0100

    Add Users class and updateUserList event

---
+++ server/utils/users.js
@@ -0,0 +1,50 @@
+class Users {
+  constructor () {
+    this.users = [];
+  }
+
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if(user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = {
+  Users
+};
+
+// class Person {    //ES6 Class definition
+//   constructor (name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} year(s) old`;
+//   }
+// }
+//
+// var me = new Person('Marco', 22);
+// console.log(me.getUserDescription());
